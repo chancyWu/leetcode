c833f56f70c6d539f9d01308d2e118ef0557557b
chancy <chancy3100@gmail.com>
2015-01-21 16:54:11 +0800

    Add excel sheet column title question description

---
+++ src/excel_sheet_column_title.py
@@ -0,0 +1,23 @@
+"""
+Source : https://oj.leetcode.com/problems/excel-sheet-column-number/
+Author : Changxi Wu
+Date   : 2015-01-21
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB
+"""
+
+# @return a string
+def convertToTitle(num):
+
+
+if __name__ == '__main__':
+    test = [1:'A', 2:'B', 3:'C', 26:'Z', 27:'AA', 28:'AB']
