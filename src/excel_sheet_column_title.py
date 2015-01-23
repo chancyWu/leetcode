c85fbf33d22a9775f9d22b863027eb50b41923c2
chancy <chancy3100@gmail.com>
2015-01-23 12:28:22 +0800

    Add solution for excel sheet column title

--- src/excel_sheet_column_title.py
+++ src/excel_sheet_column_title.py
@@ -13,11 +13,28 @@ For example:
     26 -> Z
     27 -> AA
     28 -> AB
+    732 -> ABC
 """
 
 # @return a string
 def convertToTitle(num):
-
+    remainders = []
+    quotient = num
+    while quotient > 26:
+        remainder = quotient%26 or 26
+        quotient = (quotient-remainder)/26
+        remainders.append(remainder)
+    remainders.append(quotient)
+    chars = []
+    for i in reversed(remainders):
+        chars.append(chr(i+ord('A')-1))
+    return ''.join(chars)
 
 if __name__ == '__main__':
-    test = [1:'A', 2:'B', 3:'C', 26:'Z', 27:'AA', 28:'AB']
+    test = {1:'A', 2:'B', 3:'C', 26:'Z', 27:'AA', 28:'AB', 52:'AZ', 731:'ABC'}
+    for k,v in test.iteritems():
+        output = convertToTitle(k)
+        if v != output:
+            print 'Input:', k
+            print 'Output:', output
+            print 'Expected:', v
