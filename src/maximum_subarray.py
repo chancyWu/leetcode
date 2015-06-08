0c3c1573dc3e2cd6e9b6ced9abb37ea0750ea1df
chancy <chancy3100@gmail.com>
2015-06-08 12:39:39 +0800

    Add an improved solution with prefix sum for the maximum subarray

--- src/maximum_subarray.py
+++ src/maximum_subarray.py
@@ -1,4 +1,4 @@
-# brute force solution
+# brute force solution O(n^3)
 # it works, but Time Limit Exceeded in leetcode
 class Solution1:
     # @param {integer[]} nums
@@ -15,12 +15,34 @@ class Solution1:
                     maxSum = curSum
         return maxSum
 
+# Improved brute force solution with prefix sum array O(n^2)
+# though it's better solution, but still Time Limit Exceeded in leetcode
+class Solution2:
+    # @param {integer[]} nums
+    # @return {integer}
+    def maxSubArray(self, nums):
+        if not nums:
+            return None
+        # convert array nums to prefix sum array
+        for i,num in enumerate(nums):
+            if i == 0:
+                continue
+            else:
+                nums[i] = nums[i] + nums[i-1]
+        # enumerate the sub array
+        maxSum = nums[0]
+        for size in range(1, len(nums)+1):
+            for i in range(0, len(nums) -size):
+                curSum = nums[i+size] - nums[i]
+                if curSum > maxSum:
+                    maxSum = curSum
+        return maxSum
 
 if __name__ == '__main__':
     test_list = [[-2, 1, -3, 4, -1, 2, 1, -5, 4]]
     result_list = [6]
     success = True
-    solution = Solution1()
+    solution = Solution2()
     for i,nums in enumerate(test_list):
         result = solution.maxSubArray(nums)
         if result != result_list[i]:
